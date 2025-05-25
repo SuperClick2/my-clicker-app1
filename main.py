cd5ded10b40604904038b757f6cfce43734ab3b8
SuperClick2 <robloksinovpavel@gmail.com>
2025-05-25 17:46:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -454,4 +454,4 @@ async def disconnect(name: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("game_server:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
